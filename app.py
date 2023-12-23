b9dbf8c59d93d11286a581ee71c153832e2648b2
emanuelob <oliveirabar.emanuel@gmail.com>
2023-12-22 23:46:23 -0300

    last try

--- app.py
+++ app.py
@@ -1,18 +1,10 @@
-from flask import Flask, render_template, redirect
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return render_template('index.html')
-
-# @app.route('/email')
-# def email():
-#     return redirect('http://127.0.0.1:8081/')
-
-# @app.route('/chat')
-# def chat():
-#     return redirect('http://127.0.0.1:8065/')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
